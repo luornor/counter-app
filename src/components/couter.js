de088119f2a7ca61fc3cd8a1b2f92dcbb1a69598
Tettey1 <tetteynathan89@gmail.com>
2024-04-09 11:09:46 +0000

    modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/couter.js 	new file:   src/components/style.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/components/couter.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect} from 'react';
+import './style.css'
+
+function CounterApp() {
+  // useState Hook to manage the counter value
+  const [counter, setCounter] = useState(0);
+
+  // useEffect Hook to update the document title with the counter value
+  useEffect(() => {
+    document.title = `Counter: ${counter}`;
+  }, [counter]); // runs every time the counter value changes
+
+
+  // Function to increment the counter
+  const incrementCounter = () => {
+    setCounter(prevCounter => prevCounter + 1);
+  };
+
+  // Function to decrement the counter
+  const decrementCounter = () => {
+    setCounter(prevCounter => prevCounter - 1);
+  };
+
+  return (
+    <div className='counter-container'>
+      <h1>Counter App</h1>
+      <div className='counter-value'><span>{counter}</span></div>
+      <div className='button-container'>
+      <button className='increment-button' onClick={incrementCounter}>Increment</button>
+      <button className='decrement-button' onClick={decrementCounter}>Decrement</button>
+      
+      </div>
+      
+    </div>
+  );
+}
+
+export default CounterApp;
